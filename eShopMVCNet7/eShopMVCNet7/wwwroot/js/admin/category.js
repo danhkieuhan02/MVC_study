9a4c1d79fc6fead7ac34920660c6b2be778e401b
Danh Kieu Han <danhkieuhan135@gmail.com>
2023-09-20 21:32:29 +0700

    update modal style

---
+++ eShopMVCNet7/eShopMVCNet7/wwwroot/js/admin/category.js
@@ -0,0 +1,69 @@
+﻿document.addEventListener("alpine:init", () => {
+    Alpine.data("category", () => ({
+        _list: [],
+        _modal: {},
+
+        //edit modal button
+        _modalSetting: {
+            title: "",
+            url: "",
+            primaryButtonText: ""
+        },
+
+        //edit data
+        _updinData: {
+            id: 0,
+            name: "",
+            slug: ""
+        },
+
+        init() {
+            this._modal = new bootstrap.Modal("#categoryUpdinModal");
+                this.$watch("_updinData.name", (newVal, oldVal) => {
+                    this._updinData.slug = newVal
+                        .toLowerCase()
+                        .normalize("NFD")
+                        .replace(/[\u0300-\u036f]/g, "")
+                        .replace("đ", "d")
+                        .replace(/[^a-z]/g, "-");
+                });
+
+            fetch("/Admin/Category/ListAll")
+                .then(x => x.json())
+                .then(json => {
+                    this._list = json;
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        },
+
+        //add
+        OpenModalAdd() {
+            this._modal.show();
+            this._modalSetting = {
+                title: "Add new categories",
+                url: "",
+                primaryButtonText: "Create"
+            }
+        },
+
+        //update
+        OpenModalUpdate() {
+            this._modal.show();
+            this._modalSetting = {
+                title: "Update categories",
+                url: "",
+                primaryButtonText: "Save changes"
+            }
+        },
+
+        //remove
+        /*DeleteData() {
+            this._modal = ""
+        }*/
+        SaveChanges() {
+
+        }
+    }));
+});
